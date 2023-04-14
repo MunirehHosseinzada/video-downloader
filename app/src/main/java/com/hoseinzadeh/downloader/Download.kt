a38526069bd219a73a49dfcafc344fd1554c3cec
Munireh <m.hosseinzada10@gmail.com>
2023-04-14 15:50:59 +0430

    Checking for existing videos and adding shadowJar to graddle

--- app/src/main/java/com/hoseinzadeh/downloader/Download.kt
+++ app/src/main/java/com/hoseinzadeh/downloader/Download.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import java.io.File
 import java.io.IOException
-import java.util.concurrent.TimeUnit
 
 @RequiresApi(Build.VERSION_CODES.O)
 suspend fun main() {
@@ -34,27 +33,34 @@ suspend fun main() {
             println("Directory created successfully")
         }
 
-        //Executing youtube-dl command and retrying if needed
-        doLongRunningTask(path = path)
-            .flowOn(Dispatchers.Default)
-            .retry(retries = 2) { cause ->
-                if (cause is IOException) {
-                    println("delay 30 seconds ...")
-                    delay(30000)
-                    return@retry true
-                } else {
-                    return@retry false
-                }
-            }
-            .catch {
-                // error
-                println("failed :(")
-            }
-            .collect {
-                // success
-                println("success :)")
-            }
+        val video = File("$directory/${path.lessonName}.mp4")
 
+        if(!video.exists()){
+            //Executing youtube-dl command and retrying if needed
+            doLongRunningTask(path = path)
+                .flowOn(Dispatchers.Default)
+                .retry(retries = 2) { cause ->
+                    if (cause is IOException) {
+                        println("delay 30 seconds ...")
+                        delay(30000)
+                        return@retry true
+                    } else {
+                        return@retry false
+                    }
+                }
+                .catch {
+                    // error
+                    println("failed :(")
+                }
+                .collect {
+                    // success
+                    println("success :)")
+                    val tempVideo = File("$directory/${path.lessonName}.mp4.tmp")
+                    if (tempVideo.exists()){
+                        tempVideo.renameTo(video)
+                    }
+                }
+        }
         line += 2
     }
 }
@@ -63,12 +69,12 @@ suspend fun main() {
 private fun doLongRunningTask(path: FilePath): Flow<Int> {
     return flow {
 
-        val destination = "${path.language}/${path.grade}/${path.subject}/${path.lesson}/${path.lessonName}.mp4"
+        val destination = "${path.language}/${path.grade}/${path.subject}/${path.lesson}/${path.lessonName}.mp4.tmp"
 
         val commandParts = listOf("youtube-dl", "-o", destination, path.videoLink)
         println(commandParts)
 
-//        val process = ProcessBuilder("youtube-dl", "-o", origin, path.videoLink)
+//        val process = ProcessBuilder(commandParts)
 //            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
 //            .redirectError(ProcessBuilder.Redirect.INHERIT)
 //            .start()
@@ -78,7 +84,7 @@ private fun doLongRunningTask(path: FilePath): Flow<Int> {
 //        val status = process.exitValue()
 //
 //        println(process.toString())
-
+//
 //        if (status != 0) {
 //            throw IOException()
 //        }
